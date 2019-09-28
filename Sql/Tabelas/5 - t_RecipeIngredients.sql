d4065eb54d4bcca08046cc8a99129330f2eb3f68
Patrícia Conceição <1989dias@gmail.com>
2019-09-28 16:02:35 -0300

    Tabelas Banco de Dados

---
+++ Sql/Tabelas/5 - t_RecipeIngredients.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS Recipe_Ingredients(
+  IdRecipe INT NOT NULL,
+  IdIngredient INT NOT NULL
+) ENGINE = InnoDB;
---
+++ Sql/Tabelas/6 - t_Recipe.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS Recipe(
+  IdRecipe INT AUTO_INCREMENT PRIMARY KEY,
+  IdUser INT NOT NULL,  
+  Name VARCHAR(100) NOT NULL,
+  Preparation VARCHAR(1000) NOT NULL,
+  Image BLOB NULL,
+  IdRecipeType INT NULL,
+  FOREIGN KEY (IdUser) REFERENCES Users(IdUser),
+  FOREIGN KEY (IdRecipeType) REFERENCES Recipe_Type(IdRecipeType)
+) ENGINE = InnoDB;
