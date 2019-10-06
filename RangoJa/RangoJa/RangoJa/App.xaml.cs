4c33f446e7d06e33ea490ec332ea82291c3d5ef6
marcelomenezes <marcelo.jorda@hotmail.com>
2019-10-05 21:20:13 -0300

    Primeira tela  + configuração do hot reload

--- RangoJa/RangoJa/RangoJa/App.xaml.cs
+++ RangoJa/RangoJa/RangoJa/App.xaml.cs
@@ -2,7 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+#if !DEBUG
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
 namespace RangoJa
 {
     public partial class App : Application
@@ -10,7 +12,9 @@ namespace RangoJa
         public App()
         {
             InitializeComponent();
-
+#if DEBUG
+            HotReloader.Current.Run(this);
+#endif
             MainPage = new MainPage();
         }
 
