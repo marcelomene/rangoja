033879eebe001c59a779d52c5db14c3ca10d129c
Patrícia Conceição <1989dias@gmail.com>
2019-10-09 21:46:25 -0300

    tabela Recipe_Ingredients
    Tabela atualizada para poder guardar também o tipo da unidade do ingrediente e sua quantidade.

--- Sql/Tabelas/5 - t_RecipeIngredients.sql
+++ Sql/Tabelas/5 - t_RecipeIngredients.sql
@@ -1,4 +1,9 @@
 CREATE TABLE IF NOT EXISTS Recipe_Ingredients(
   IdRecipe INT NOT NULL,
-  IdIngredient INT NOT NULL
+  IdIngredient INT NOT NULL,
+  IdUnitType INT NULL,
+  Amount VARCHAR(30) NULL,
+  FOREIGN KEY (IdRecipe) REFERENCES Recipe(IdRecipe),
+  FOREIGN KEY (IdIngredient) REFERENCES Ingredients(IdIngredient),
+  FOREIGN KEY (IdUnitType) REFERENCES Units(IdUnitType)
 ) ENGINE = InnoDB;
