19f004454761f4a52ef38a077d75ded5b93c87bd
Patrícia Conceição <1989dias@gmail.com>
2019-10-23 21:11:09 -0300

    Update tabela Recipe

--- Sql/Tabelas/6 - t_Recipe.sql
+++ Sql/Tabelas/6 - t_Recipe.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS Recipe(
   IdRecipe INT AUTO_INCREMENT PRIMARY KEY,
-  IdUser INT NOT NULL,  
+  IdUser INT NOT NULL, 
+  IdApplianceType INT NULL,
   Name VARCHAR(100) NOT NULL,
   Preparation VARCHAR(1000) NOT NULL,
   Image BLOB NULL,
@@ -8,5 +9,6 @@ CREATE TABLE IF NOT EXISTS Recipe(
   Portion VARCHAR(30) NULL,
   PreparationTime VARCHAR(10) NULL,
   FOREIGN KEY (IdUser) REFERENCES Users(IdUser),
-  FOREIGN KEY (IdRecipeType) REFERENCES Recipe_Type(IdRecipeType)
+  FOREIGN KEY (IdRecipeType) REFERENCES Recipe_Type(IdRecipeType),
+  FOREIGN KEY (IdApplianceType) REFERENCES Appliance_Type(IdApplianceType)
 ) ENGINE = InnoDB;
