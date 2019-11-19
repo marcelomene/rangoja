1d7883e156780281e24ca735cc7f432a67c22d69
marcelomenezes <marcelo.jorda@hotmail.com>
2019-11-18 23:31:20 -0200

    arquuivos faltantes

---
+++ RangoJa/RangoJa/RangoJa/Utils.cs
@@ -0,0 +1,18 @@
+﻿using RangoJaDatabaseAccess.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangoJa
+{
+    public class Utils
+    {
+        public static async void DisplayDialog(string title, string msg)
+            => await App.Current.MainPage.DisplayAlert(title, msg, "OK");
+
+        public static List<Ingredient> AllIngridients { get; set; }
+        public static List<Unit> AllUnits { get; set; }
+        public static List<ApplianceType> AllAppliances { get; set; }
+        public static List<RecipeType> AllRecipeTypes { get; set; }
+    }
+}
